82fe5b5e488750cb8a8ae4239ba0515dd563435c
Noah <novu@chapman.edu>
2025-01-29 19:56:44 -0800

    Added Player.cs

---
+++ Cpsc370Final/Player.cs
@@ -0,0 +1,19 @@
+namespace Cpsc370Final;
+
+public class Player
+{
+    public String userName;
+    public Pokemon playersPokemon;
+    private bool isPlayerOne;
+
+    public void getUsername()
+    {
+        /* TODO: get user input, can only be letters or numbers, if not keep asking for user input, until they enter a valid name*/
+        // then set the variable userName to the userInput
+    }
+
+    public void getPlayersPokemon()
+    {
+        playersPokemon.createPokemon();
+    }
+}
